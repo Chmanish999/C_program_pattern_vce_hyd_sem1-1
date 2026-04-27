0dc409bf9a89d562aa89db0d61411d7cdab25dbc
Ch Manish <98759715+Chmanish999@users.noreply.github.com>
2026-04-27 23:18:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
 function generatePattern() {
-
-    let ch = document.getElementById("charInput").value.toUpperCase(); 
     let output = "";
 
     for (let i = 1; i <= 5; i++) {
         for (let j = 1; j <= 5; j++) {
 
-            let printStar = false;
-
-            switch(ch) {
-
-                case 'A':
-                    if (j === 3 || i === 3 || (j === 1 && i > 1) || (j === 5 && i > 1))
-                        printStar = true;
-                    break;
-
-                case 'B':
-                    if (j === 1 || ((i === 1 || i === 3 || i === 5) && j < 5) || (j === 5 && (i === 2 || i === 4)))
-                        printStar = true;
-                    break;
-
-                case 'C':
-                    if (i === 1 || i === 5 || j === 1)
-                        printStar = true;
-                    break;
-
-                default:
-                    output = "Invalid Input";
-                    document.getElementById("output").textContent = output;
-                    return;
+            if (i === 1 || i === 5 || j === 1) {
+                output += "* ";
+            } else {
+                output += "  ";
             }
 
-            output += printStar ? "* " : "  ";
         }
         output += "\n";
     }
